3096a552da16541b9c927facc031e3977eccc57f
cjewell47 <charles.jewell47@gmail.com>
2018-10-20 19:53:48 +0100

    events

--- vue-js-playlist/app.js
+++ vue-js-playlist/app.js
@@ -2,11 +2,20 @@ new Vue({
   el: '#vue-app',
   data: {
     name: 'Big C',
-    job: 'Baller'
+    job: 'Baller',
+    website: 'https://www.charlesjewell.com',
+    websiteTag: '<a href="https://www.charlesjewell.com">Here</a>',
+    age: 25
   },
   methods: {
     greet: function(people) {
       return 'What is up my ' + people + ', ' + this.name + ' is in the building';
+    },
+    add: function(inc) {
+      this.age += inc;
+    },
+    subtract: function(dec) {
+      this.age -= dec;
     }
   }
 });
